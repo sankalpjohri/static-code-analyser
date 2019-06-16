9505b48ff09516ec9966518ac44e0c278630d09b
Sankalp Johri <sankalp.johri@iiitb.org>
2019-06-16 22:12:56 +0530

    Created projects

---
+++ static-code-analyser-backend/src/main/java/com/sankalp/staticcodeanalyserbackend/StaticCodeAnalyserBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sankalp.staticcodeanalyserbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticCodeAnalyserBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticCodeAnalyserBackendApplication.class, args);
+	}
+
+}
---
+++ static-code-analyser-backend/src/test/java/com/sankalp/staticcodeanalyserbackend/StaticCodeAnalyserBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sankalp.staticcodeanalyserbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StaticCodeAnalyserBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
